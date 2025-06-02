5a333c8b79415e5530d4390315a00ac0dc13a968
imgengineer <lzc011210@163.com>
2025-06-02 22:33:27 +0800

    fix some issue

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import jax
 from flax import nnx
-from models.vision_transformer import vit_b_16
 from models.shufflenetv2 import  shufflenet_v2_x2_0
-import jax.numpy as jnp
 
 rngs = nnx.Rngs(0)
 x = jax.random.normal(rngs.params(), (1, 224, 224, 3))
